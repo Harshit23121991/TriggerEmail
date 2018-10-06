57954c341e4e6040442b18b501a2618e57893ea4
Harshit <harshitacharya.kadekaru@itcinfotech.com>
2018-10-06 11:23:13 +0530

    first commit

---
+++ src/main/java/com/email/TriggerEmail/TriggerEmailApplication.java
@@ -0,0 +1,12 @@
+package com.email.TriggerEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriggerEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriggerEmailApplication.class, args);
+	}
+}
